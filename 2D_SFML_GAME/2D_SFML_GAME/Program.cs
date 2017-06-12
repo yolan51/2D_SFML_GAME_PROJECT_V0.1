0df88e9062a293f8759d97c7c95d5baf147319ba
Yohann Pruneau <yolan51@hotmail.com>
2017-06-12 18:52:16 -0400

    First SFML

---
+++ 2D_SFML_GAME/2D_SFML_GAME/Program.cs
@@ -0,0 +1,31 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using SFML.Graphics;
+    using SFML.Window;
+
+
+    namespace SFML._2D_SFML_GAME
+    {
+        class Program
+        {
+            static void Main()
+            {
+                RenderWindow window = new RenderWindow(new Window.VideoMode(200, 200), "test");
+                CircleShape cs = new CircleShape(100.0f);
+                cs.FillColor = Color.Green;
+                window.SetActive();
+                while (window.IsOpen)
+                {
+                    window.Clear();
+                    window.DispatchEvents();
+                    window.Draw(cs);
+                    window.Display();
+                }
+            }
+        }
+    }
+
