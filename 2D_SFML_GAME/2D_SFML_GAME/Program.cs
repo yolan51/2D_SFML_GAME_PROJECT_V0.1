a616cb5bd31047dc690fbbe7df8b40e6ea0c3b67
Yohann Pruneau <yolan51@hotmail.com>
2017-06-12 19:18:25 -0400

    SimpleVersion With DebugMode

--- 2D_SFML_GAME/2D_SFML_GAME/Program.cs
+++ 2D_SFML_GAME/2D_SFML_GAME/Program.cs
@@ -1,31 +1,59 @@
-﻿
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using SFML.Graphics;
-    using SFML.Window;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using System.Runtime.InteropServices;
 
 
-    namespace SFML._2D_SFML_GAME
+namespace SFML._2D_SFML_GAME
+{
+
+    class Program
     {
-        class Program
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+      
+
+        private static Boolean NoDebugMode = true;
+        static void Main()
         {
-            static void Main()
+            SwitchDebugMode(NoDebugMode);
+             RenderWindow window = new RenderWindow(new Window.VideoMode(200, 200), "test");
+            CircleShape cs = new CircleShape(100.0f);
+            cs.FillColor = Color.Green;
+            window.SetActive();
+
+            while (window.IsOpen)
             {
-                RenderWindow window = new RenderWindow(new Window.VideoMode(200, 200), "test");
-                CircleShape cs = new CircleShape(100.0f);
-                cs.FillColor = Color.Green;
-                window.SetActive();
-                while (window.IsOpen)
-                {
-                    window.Clear();
-                    window.DispatchEvents();
-                    window.Draw(cs);
-                    window.Display();
-                }
+                window.Clear();
+                window.DispatchEvents();
+                window.Draw(cs);
+                window.Display();
             }
         }
-    }
+
+        static private void  SwitchDebugMode(Boolean NoDebugMode){
+            if (NoDebugMode)
+            {
+                var handle = GetConsoleWindow();
+                ShowWindow(handle, SW_HIDE);
+
+            }
+        }
+
+
+    }   
+
+
+}
+    
 
