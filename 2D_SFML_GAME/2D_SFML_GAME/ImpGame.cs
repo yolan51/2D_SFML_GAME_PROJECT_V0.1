1e5fd9c3a7f3f265497ea6d15e9dd124dfc7e9f3
Yohann Pruneau <yolan51@hotmail.com>
2017-06-13 14:58:12 -0400

    Origin/master (#1)
    * Initial commit
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Create README.md
    
    * Set theme jekyll-theme-cayman
    
    * Delete _config.yml
    
    * Create README.md
    
    * Empty SFML Sample
    
    * First SFML
    
    * Delete .gitignore
    
    * Delete .gitattributes
    
    * SimpleVersion With DebugMode
    
    * Hided Console
    
    * Ajoutez les fichiers .gitignore et .gitattributes.
    
    * Ajoutez des fichiers projet.
    
    * Testing push
    
    * Create README.md
    
    * Allright
    
    * Valide
    
    * Work fine
    
    * Delete of 4 extra dll
    
    * Delete .suo
    
    * BetterSimplerSFML
    
    * WTf
    
    * Ajoutez des fichiers projet.
    
    * Testing push

---
+++ 2D_SFML_GAME/2D_SFML_GAME/ImpGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace _2D_SFML_GAME
+{
+    class ImpGame : Game
+    {
+        public ImpGame():base(900,600,"RPG",Color.Cyan){
+
+        }
+        protected override void Initialize()
+        {
+           
+        }
+
+        protected override void LoadContent()
+        {
+          
+        }
+
+        protected override void Render()
+        {
+            
+        }
+
+        protected override void Tick()
+        {
+          
+        }
+    }
+}
---
+++ 2D_SFML_GAME/2D_SFML_GAME/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using System.Runtime.InteropServices;
+using _2D_SFML_GAME;
+
+namespace Controler
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            ImpGame mygame = new ImpGame();
+            mygame.Run();
+        }
+
+    }
+    
+}
+    
